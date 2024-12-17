d421b89cba633df07c8403adc2f1e5ee081e6404
Kevin Lam <klam@lcs.on.ca>
2024-12-17 09:18:03 -0500

    Set up the frame work for coding

---
+++ Kevin's Hobby Cards/Model/detailedView.swift
@@ -0,0 +1,18 @@
+//
+//  detailedView.swift
+//  Kevin's Hobby Cards
+//
+//  Created by Kevin Lam on 17/12/2024.
+//
+
+import SwiftUI
+
+struct detailedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    detailedView()
+}
