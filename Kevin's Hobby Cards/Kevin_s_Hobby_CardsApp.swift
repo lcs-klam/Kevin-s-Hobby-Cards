c96f221293664958b22becdbff94b30bd7250a39
Kevin Lam <klam@lcs.on.ca>
2024-12-17 08:50:00 -0500

    Initial Commit

---
+++ Kevin's Hobby Cards/Kevin_s_Hobby_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kevin_s_Hobby_CardsApp.swift
+//  Kevin's Hobby Cards
+//
+//  Created by Kevin Lam on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Kevin_s_Hobby_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
